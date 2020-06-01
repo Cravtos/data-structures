f557d10f4daeb99971b36200f22330e9e12279ac
Игорь <59143800+Koynovigor@users.noreply.github.com>
2020-06-01 20:29:25 +0700

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,6 +1,12 @@
-#include "stack.h"
-
 #include <stdio.h>
+#include "linked_list.c"
+
+struct stack
+{
+    linked_list *ll;
+};
+
+typedef struct stack stack;
 
 stack stack_new()
 {
@@ -22,4 +28,39 @@ int stack_pop(stack s)
 int stack_len(stack s)
 {
     return len(s.ll);
+}
+
+struct stack_queue
+{
+    stack a;
+    stack b;
+};
+
+struct stack_queue stack_queue_new()
+{
+    struct stack_queue queue =
+    {
+        .a = stack_new(),
+        .b = stack_new()
+    };
+    return queue;
+}
+
+int stack_queue_pop(struct stack_queue s)
+{
+    while (stack_len(s.a) != 0)
+    {
+        stack_push(s.b, stack_pop(s.a));
+    }
+    return stack_pop(s.b);
+}
+
+void stack_queue_push(struct stack_queue queue, int a)
+{
+    stack_push(queue.a, a);
+}
+
+int stack_queue_len(struct stack_queue s)
+{
+    return stack_len(s.a) + stack_len(s.b);
 }
