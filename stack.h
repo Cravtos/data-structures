dfa20283e6005357cfe432dd354d926d633c50ac
Игорь <59143800+Koynovigor@users.noreply.github.com>
2020-06-01 20:33:31 +0700

    Update stack.h

--- stack.h
+++ stack.h
@@ -16,5 +16,16 @@ void stack_push(stack s, int element);
 int stack_pop(stack s);
 int stack_len(stack s);
 
+struct stack_queue
+{
+    stack a;
+    stack b;
+};
+
+struct stack_queue stack_queue_new();
+int stack_queue_len(struct stack_queue s);
+void stack_queue_push(struct stack_queue queue, int a);
+int stack_queue_pop(struct stack_queue s);
+
 
 #endif //DATA_STRUCTURES__STACK_H
